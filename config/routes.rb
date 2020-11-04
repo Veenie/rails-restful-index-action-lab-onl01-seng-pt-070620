5d711902176fc6a6f825d4757ab3fdbc2ae75981
Raveen K Sharma <raveen08@gmail.com>
2020-11-04 20:55:22 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :students, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  get 'students', to: 'students#students'
+  
 end
